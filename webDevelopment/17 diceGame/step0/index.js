58d13bb11f3e8c8a38b43e11909dfbfced7c1525
Limbuarin <limbuaneel11@gmail.com>
2025-11-20 16:37:24 +0545

    added new exercise dicegame

---
+++ webDevelopment/17 diceGame/step0/index.js
@@ -0,0 +1,33 @@
+let randomNumber1 = Math.floor(Math.random() * 6 ) + 1; // +1 means shift the value by 1
+let randomNumber2 = Math.floor(Math.random() * 6 ) + 1; // +1 means shift the value by 1
+// console.log(randomNumber1);
+
+// left
+const img1 = document.querySelector(".img1")
+    .setAttribute("src", "images/dice"+randomNumber1+".png")
+
+
+// right
+const img2 = document.querySelector(".img2")
+    .setAttribute("src", "images/dice"+randomNumber2+".png")
+
+console.log(img1);
+function roll(random1, random2){
+    let result = document.querySelector("h1");
+    if(random1 > random2){
+        result.innerHTML = `
+            🚩player 1 wins!
+        `
+    }else if (random1 < random2){
+        result.innerHTML = `
+            player2 wins!🚩
+        `
+    }else {
+        result.innerHTML = "Draw!"
+    }
+
+    result.textContent = "Refresh Me";
+
+}
+
+roll(randomNumber1, randomNumber2);
