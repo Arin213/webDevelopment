a29eaf34518bfca3798e12a11c2c24cd311bfa80
Limbuarin <limbuaneel11@gmail.com>
2025-11-20 16:41:00 +0545

    made changes index.js

--- webDevelopment/17 diceGame/step0/index.js
+++ webDevelopment/17 diceGame/step0/index.js
@@ -1,19 +1,18 @@
 let randomNumber1 = Math.floor(Math.random() * 6 ) + 1; // +1 means shift the value by 1
 let randomNumber2 = Math.floor(Math.random() * 6 ) + 1; // +1 means shift the value by 1
-// console.log(randomNumber1);
 
 // left
 const img1 = document.querySelector(".img1")
     .setAttribute("src", "images/dice"+randomNumber1+".png")
-
 
 // right
 const img2 = document.querySelector(".img2")
     .setAttribute("src", "images/dice"+randomNumber2+".png")
 
 console.log(img1);
+let result = document.querySelector("h1");
 function roll(random1, random2){
-    let result = document.querySelector("h1");
+   
     if(random1 > random2){
         result.innerHTML = `
             🚩player 1 wins!
@@ -25,9 +24,7 @@ function roll(random1, random2){
     }else {
         result.innerHTML = "Draw!"
     }
-
-    result.textContent = "Refresh Me";
-
+    
 }
 
 roll(randomNumber1, randomNumber2);
