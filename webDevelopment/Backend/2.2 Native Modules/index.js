2895d4879393986094969f1a818304e3a7b7b74e
Limbuarin <limbuaneel11@gmail.com>
2026-05-18 22:29:13 +0545

    Added the backend file folder [begining of the course]

---
+++ webDevelopment/Backend/2.2 Native Modules/index.js
@@ -0,0 +1,13 @@
+// const fs = require("fs");
+
+// fs.writeFile("Message.txt", "Hello from Node.js!", (err) => {
+//     if(err) throw err;
+//     console.log("The file has been saved!");
+// });
+
+const fs = require("fs");
+fs.readFile("Message.txt","utf8", (err,data) =>{
+    if (err) throw err;
+    console.log(data);
+
+});
